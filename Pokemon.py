bba3680cee469dd02d61ce284cebe9a5ea442293
Peter Robe <probe191919@gmail.com>
2018-07-19 00:59:24 +0900

    Implemented dodge mechanic and cleaned up damage/crit calculation code

--- Pokemon.py
+++ Pokemon.py
@@ -5,8 +5,9 @@ class Pokemon(Entity):
     """
         Any Entity that can battle is a Pokemon (Player, Monster)
     """
+    critMultiplier = 1.5
 
-    def __init__(self, overworld_x, overworld_y, sprites_path, overworldChar, arena_x, arena_y, defensePower, health,  crit, moveset):
+    def __init__(self, overworld_x, overworld_y, sprites_path, overworldChar, arena_x, arena_y, defensePower, health, evade, crit, moveset):
         self.neutralSprite = []
         self.startAttackSprite = []
         self.endAttackSprite = []
@@ -18,30 +19,57 @@ class Pokemon(Entity):
         self.defensePower = defensePower
         self.maxHealth = health
         self.currentHealth = health
+        self.evade = evade
         self.crit = crit
         self.moveset = moveset
         self.nextAttackDamage = 0
+        self.nextAttackCrits = False
         self.nextAttackHits = True
+
+    def calcHit(self, attack, target):
+        """
+        Calculate dodge. Saves it to self.nextAttackHits
+        """
+        finalHitChance = attack.hitChance * (1 - target.evade)
+        prob = random.randint(0, 100) / 100
+        if finalHitChance >= prob:
+            self.nextAttackHits = True
+        else:
+            self.nextAttackHits = False
+
+    def calcDamage(self, attack, target):
+        """
+        Calculates damage and crit. Saves it to self.nextAttackDamage and self.nextAttackCrits
+        """
+        damage = int(attack.damage * attack.damage / (attack.damage + target.defensePower))
+
+        # critical chance calculations
+        prob = random.randint(0, 100) / 100
+        if self.crit >= prob:
+            self.nextAttackCrits = True
+            damage = damage * Pokemon.critMultiplier
+        else:
+            self.nextAttackCrits = False
+        self.nextAttackDamage = damage
 
     def calcAttack(self, attack, target):
         #  Calculate damage that is to be applied to the target in the future.
         # damage = att * att / (att + def)
         # https://gamedev.stackexchange.com/questions/129319/rpg-formula-attack-and-defense
-        damage = int(attack.damage * attack.damage / (
-                attack.damage + target.defensePower))
-        # implrment critical chance and damage
-        prob = random.randint(0, 100) / 100
-        if prob <= self.crit:
-            print("we crit")
-            damage = damage * 1.5
-        self.nextAttackDamage = damage
-        self.applyStatus(attack, target)
+        self.calcHit(attack, target)
+        if self.nextAttackHits:
+            self.calcDamage(attack, target)
+        else:
+            self.nextAttackCrits = False
+            self.nextAttackDamage = 0
 
-    def applyAttack(self, target):
-
-
+    def applyAttack(self, attack, target):
         #  Apply damage to monster.
-        target.currentHealth -= self.nextAttackDamage
+        if self.nextAttackHits:
+            target.currentHealth -= self.nextAttackDamage
+            #self.applyStatus(attack, target)
+
+    #TODO: self.applyStatus()
 
     def loadSprites(self, filename):
         """Loads sprites from a .txt file and assigns them to Pokemon's attributes.
